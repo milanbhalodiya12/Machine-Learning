dd6b26041563cec2c5a8ba76d2e50dca0ceefac4
milanbhalodiya12 <143157260+milanbhalodiya12@users.noreply.github.com>
2024-06-28 11:28:07 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread("4.png", cv2.IMREAD_COLOR)
+
+face = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = face.detectMultiScale(grey, 1.1, 4)
+print(faces)
+
+for(x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y), (x+w,y+h), (0,255,0),3)
+
+cv2.imshow("Face Detector",img)
+cv2.waitKey(50000)
+cv2.destroyAllWindows()
